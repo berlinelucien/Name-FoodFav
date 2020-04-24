6526e8a3c69a997d565a10f73e2de4fcdd6194d1
Puma Thongsithavong <v-photho@microsoft.com>
2020-04-23 23:12:41 -0700

    basic-navigation: Migrated MenuContainer to App

--- Week_2/basic_navigation/src/App.tsx
+++ Week_2/basic_navigation/src/App.tsx
@@ -1,13 +1,50 @@
 import React from 'react'
 import './App.css'
-import MenuContainer from './menu/MenuContainer'
+import MenuButton from "./menu/MenuButton"
+import Menu from "./menu/Menu"
 
-function App() {
-  return (
-    <div className="App">
-      <MenuContainer />      
-    </div>
-  );
+interface MenuContainerProps {
+  // MenuContainer Properties
+}
+
+interface MenuConstainerState {
+  visible: boolean
+}
+
+class App extends React.Component {
+  state: MenuConstainerState
+
+  constructor(props: MenuContainerProps) {
+    super(props)
+
+    this.state = { visible: false }
+
+    this.handleMouseDown = this.handleMouseDown.bind(this)
+    this.toggleMenu = this.toggleMenu.bind(this)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <MenuButton handleMouseDown={this.handleMouseDown} />
+        <Menu
+          handleMouseDown={this.handleMouseDown}
+          menuVisibility={this.state.visible}
+        />
+      </div>
+    )
+  }
+
+  handleMouseDown(e: MouseEvent) {
+    this.toggleMenu()
+    e.stopPropagation()
+  }
+
+  toggleMenu() {
+    this.setState({
+      visible: !this.state.visible,
+    })
+  }
 }
 
 export default App
