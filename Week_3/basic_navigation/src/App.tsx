101fd08b1690865b6a518a05a7a38c0ce58f7667
Puma Thongsithavong <v-photho@microsoft.com>
2020-04-27 22:51:52 -0700

    page-routing: added basic routing to simple pages.\

--- Week_3/basic_navigation/src/App.tsx
+++ Week_3/basic_navigation/src/App.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import MenuButton from "./menu/MenuButton";
 import Menu from "./menu/Menu";
+import { Route, BrowserRouter } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import AccessoriesPage from "./pages/AccessoriesPage";
+import SettingsPage from "./pages/SettingsPage";
 
 export interface MenuContainerProps {
   menuVisible: boolean;
@@ -16,13 +21,21 @@ class App extends React.Component<MenuContainerProps> {
 
   render() {
     return (
-      <div className="App">
-        <MenuButton handleMouseDown={this.handleMouseDown} />
-        <Menu
-          handleMouseDown={this.handleMouseDown}
-          menuVisibility={this.props.menuVisible}
-        />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <MenuButton handleMouseDown={this.handleMouseDown} />
+          <Menu
+            handleMouseDown={this.handleMouseDown}
+            menuVisibility={this.props.menuVisible}
+          />
+          <div className="content">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/products" component={ProductPage} />
+            <Route path="/accessories" component={AccessoriesPage} />
+            <Route path="/settings" component={SettingsPage} />
+          </div>
+        </div>
+      </BrowserRouter>
     );
   }
 
--- Week_3/basic_navigation/src/menu/Menu.tsx
+++ Week_3/basic_navigation/src/menu/Menu.tsx
@@ -1,34 +1,34 @@
-import React from "react"
-import "./Menu.css"
+import React from "react";
+import "./Menu.css";
+import { NavLink } from "react-router-dom";
 
 interface MenuProps {
-  handleMouseDown: any
-  menuVisibility: boolean
-};
+  handleMouseDown: any;
+  menuVisibility: boolean;
+}
 
 export default class Menu extends React.Component<MenuProps> {
   render() {
-    var className = this.props.menuVisibility ? "show" : "hide"
+    var className = this.props.menuVisibility ? "show" : "hide";
 
     return (
-      <div
-        id="sideMenu"
-        onMouseDown={this.props.handleMouseDown}
-        className={className}
-      >
-        <h2>
-          <a href="#">Home</a>
-        </h2>
-        <h2>
-          <a href="#">Products</a>
-        </h2>
-        <h2>
-          <a href="#">Accessories</a>
-        </h2>
-        <h2>
-          <a href="#">Settings</a>
-        </h2>
+      <div id="sideMenu" className={className}>
+          <NavLink to="/" onClick={this.props.handleMouseDown}>
+            Home
+          </NavLink>
+       
+          <NavLink to="/products" onClick={this.props.handleMouseDown}>
+            Products
+          </NavLink>
+        
+          <NavLink to="/accessories" onClick={this.props.handleMouseDown}>
+            Accessories
+          </NavLink>
+        
+          <NavLink to="/settings" onClick={this.props.handleMouseDown}>
+            Settings
+          </NavLink>
       </div>
-    )
+    );
   }
 }
