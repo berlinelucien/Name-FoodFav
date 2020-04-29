25131ab6d2588a7153d7aa536c56bf049445670a
Puma Thongsithavong <v-photho@microsoft.com>
2020-04-29 13:40:58 -0700

    view-component-layout: Added KeywordCollection component

--- Week_2/view-component-layout/src/App.tsx
+++ Week_2/view-component-layout/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import KeywordCollection from "./keyword-collection/KeywordCollection";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div id="keywordCollection">
+        <KeywordCollection words={this.words()} />
+      </div>
+    );
+  }
+
+  words(): string[] {
+    return [
+      "one",
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "eight",
+      "nine",
+      "ten",
+      "eleven",
+      "twelve",
+      "thirteen",
+      "fourteen",
+      "fifteen",
+      "sixteen",
+      "seventeen",
+      "eighteen",
+      "nineteen",
+      "twenty"
+    ];
+  }
 }
-
-export default App;
