4b43718dbec8b64aa49aac0a6973323ea32b5c97
Puma Thongsithavong <v-photho@microsoft.com>
2020-04-30 18:33:29 -0700

    fluent-ui: Added variable to easily load however number of items in the feed.

--- Week_3/fluent-ui/src/DataProvider.ts
+++ Week_3/fluent-ui/src/DataProvider.ts
@@ -2,11 +2,13 @@ import { FeedContentData, FeedProfileViewData, FeedItemData } from './Feed/FeedD
 
 export default class DataProvider {
     static getData(): FeedItemData[] {
+        const numberOfItems = 25
+
         const imageUrl = new URL("https://vignette.wikia.nocookie.net/harrypotter/images/a/aa/Voldemort-smiling.jpg/revision/latest/scale-to-width-down/340?cb=20180920185319")
         const contentUrl = new URL("https://live.staticflickr.com/4828/31281889047_e930645d2d_b.jpg")
         const contentData = new FeedContentData(contentUrl)
         const profileData = new FeedProfileViewData(imageUrl, 'Tom Riddle', 'Seattle, WA')
         const itemData = new FeedItemData(profileData, contentData)
-        return Array(25).fill(itemData)
+        return Array(numberOfItems).fill(itemData)
     }
 }
