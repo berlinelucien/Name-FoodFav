5665b73014df583d92dd18a957486d3dce4698c5
Christelle Scharff <scharffc@gmail.com>
2020-07-12 18:18:53 +0000

    Add files via upload
    New set of files for the redux-simple-counter with TODOs

---
+++ Week_2/redux-simple-counter/src/src/redux-components/actions.tsx
@@ -0,0 +1,14 @@
+// Actions and their types
+
+const INCREASE = "INCREASE";
+const DECREASE = "DECREASE";
+
+export let decreaseAction = {
+    type: DECREASE
+};
+
+export let increaseAction = {
+    type: INCREASE
+};
+
+
