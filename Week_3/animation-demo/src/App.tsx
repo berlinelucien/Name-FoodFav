b438d5c6241068f5d443496f4e3065bffe0277cb
Bansenauer <v-brban@microsoft.com>
2020-07-15 15:44:23 -0700

    cleanup

--- Week_3/animation-demo/src/App.tsx
+++ Week_3/animation-demo/src/App.tsx
@@ -1,17 +1,43 @@
 import React from "react";
 import './App.css';
-import {ShakeImage} from './components/ShakeImage'
-import RevealView from './components/RevealView'
+import {FlyImage} from './components/FlyImage'
+import { useSpring, animated } from 'react-spring';
 
-function App() {
+
+const App = () => {
+  const leftAnimation = useSpring({
+    from: { marginLeft: -5000 },
+    to: { marginLeft: 0 },
+    delay:100
+  });
+
+  const rightAnimation = useSpring({
+    from: { marginRight: -5000 },
+    to: { marginRight: 0 },
+    delay:1000
+  });
+
+  const multiAnimation = useSpring({
+    from: { opacity: 0, color: 'red' },
+    to: { opacity: 1, color: '#ffaaee' },
+    config: {duration : 4000},
+    delay:2000
+ 
+  });
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <ShakeImage />
+        <FlyImage />
       </header>
-      <RevealView />
+      <animated.div style={leftAnimation}>Learn </animated.div> 
+      <animated.div style={rightAnimation}>Animation </animated.div>
+      <animated.a style={multiAnimation}
+                  className="App-link"
+                  href="https://www.react-spring.io/"
+                  target="_blank"
+                  rel="noopener noreferrer">(Demos)</animated.a>
     </div>
-  );
-}
+    );
+  }
 
-export default App;
+  export default App;
