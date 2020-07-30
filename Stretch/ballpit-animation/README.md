6ba9a044479afcde02c1f08f7970a022a0284165
Bansenauer <v-brban@microsoft.com>
2020-07-30 15:14:17 -0700

    adds ballpit simulation

---
+++ Stretch/ballpit-animation/README.md
@@ -0,0 +1,4 @@
+## Ballpit Simulation using Matter 2D Physics Engine Component
+
+This app uses the [React-DOM-Box2d](https://github.com/oneto018/react-dom-box2d)  Component Library based on the [Matter.js 2D physics engine](https://brm.io/matter-js) to animate the movement of the DOM "[Items](ttps://oneto018.github.io/react-dom-box2d/#/Item/ )" inside its "[World](ttps://oneto018.github.io/react-dom-box2d/#/World/ )"
+
---
+++ Stretch/ballpit-animation/src/App.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import { World, Item } from "react-dom-box2d";
+import logo from './logo.svg';
+import './App.css';
+
+interface ball {
+  id:number
+  bounce: number
+  shape: string
+  size: number
+  position:{left:number, top:number}
+  force: {x:number, y:number}
+}
+/* Ball Data an array of random ball objects based on Dom-Box2d Item
+   https://oneto018.github.io/react-dom-box2d/#/Item/
+*/
+const generateItems = (n:number) => {
+  const random = Array.from({length: n}, () => Math.random());
+  let balls:ball[]=[];
+  random.map((random, index)=>{
+    const sign = (Math.random()<.5) ? 1 : -1; // used to set a random positive or negative direction
+    const sign2= (Math.random()<.5) ? -1 : 1; // in for both x and y coordinates
+    balls.push(
+      {
+        id:index,
+        bounce: random*1.15,
+        shape: "circle",
+        size: 20*random + sign*10 + 100,
+        position: {left:1600*random, top:800*random},
+        force: {x:30*random*sign, y:50*random*sign2}
+      }
+    )
+  })
+  return balls;
+}
+
+class App extends React.Component<{}, {}> {
+  ballPit:ball[];
+  constructor(props){
+    super(props);
+    this.ballPit = generateItems(60); /* Generate items for the simulated world */
+  }
+ 
+  render(){
+    return (
+      <div className="App">
+        <h1>Ball Pit Simulaton using &nbsp;
+          <a href="https://github.com/oneto018/react-dom-box2d" target="_blank" rel="noopener noreferrer">
+            DOM-Box2d React Component Library</a></h1>
+        <main className="App-header">
+        {/* Physics world starts here as described in Dom-Box2d World
+            https://oneto018.github.io/react-dom-box2d/#/World/ 
+            
+            You could make this a separate component, passing in World parameters and ball data as props
+        */}
+        <World
+          height={800}
+          width={1600}
+          gravity={[0,4]}
+          className="world"
+        >
+          {/* Create each item using the item data */
+            this.ballPit.map(ball => (
+              <Item
+                key={ball.id}
+                restitution={ball.bounce}
+                left={ball.position.left}
+                top={ball.position.top}
+                height={ball.size}
+                width={ball.size}
+                shape={ball.shape}
+                initialImpulse={[ball.force.x, ball.force.y]} 
+              >
+                {/* Each "Item" can be any element you want. Once it's inside the "World"*/}
+                <img src={logo} alt="logoBall" 
+                     style={{border:2, borderStyle:"solid", borderColor:"cyan"}}/>
+              </Item>
+          ))}
+        </World>  
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
